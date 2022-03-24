9fbee9e4f4f80137aad0ce69a41e79de55c38ae2
onurcpgl <onurcpgl.34@hotmail.com>
2022-03-24 18:21:35 +0300

    Operatörler

---
+++ Operatörler/Operatörler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            //Toplama +
+            a++;//artırma işlemi
+            int sonuc = a + b; 
+            int sonuc2 = a * b;//Çarpam
+            int sonuc3 = a / b;//Bölme
+            if (b==a)//Bool döndürür eşitse true eşit değilse false döndürür.
+            {
+                Console.WriteLine("Eşit değildir.");
+
+            }
+            a--;//azaltma işlemi 1 çıkarır ++ 1 artırır..
+
+        }
+    }
+}
