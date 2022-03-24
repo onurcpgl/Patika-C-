e186cb86f80b831f48a9cb8f3fa33fa558b1724c
onurcpgl <onurcpgl.34@hotmail.com>
2022-03-24 20:50:03 +0300

    Dongüler-Break-Continue

---
+++ Dongüler-Break-Continue/Dongüler-Break-Continue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dongüler_Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("10 defa ekrana yazdırır");
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("10 defa ekrana yazdırır");
+                if (i==4)
+                {
+                    break; // i=4 ise döngüden çıkarır.
+                }
+                continue; // Devam ettir. Durma devam et demektir.
+            }
+        }
+    }
+}
--- Karar Yapıları/Karar Yapıları/Program.cs
+++ Karar Yapıları/Karar Yapıları/Program.cs
@@ -54,6 +54,8 @@ namespace Karar_Yapıları
                     Console.WriteLine("Pazar");
                     break;
 
+
+
             }
 
         }
