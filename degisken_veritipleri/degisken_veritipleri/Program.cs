474072a7c2b69989892bb0242b22ff9914ec424a
onurcpgl <onurcpgl.34@hotmail.com>
2022-03-24 17:53:37 +0300

    Initial commit

---
+++ degisken_veritipleri/degisken_veritipleri/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace degisken_veritipleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 10;
+            string ad = "onur";
+            int yas=23;
+        }
+    }
+}
