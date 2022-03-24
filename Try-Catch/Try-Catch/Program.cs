7838c9a9871aa6a4d3cb069299a19a23f77fd9a1
onurcpgl <onurcpgl.34@hotmail.com>
2022-03-24 18:52:19 +0300

    Try-Catch

---
+++ Try-Catch/Try-Catch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
