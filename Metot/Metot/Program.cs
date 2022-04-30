d6479b6f363fc30bb75bb46a9374394fa4727531
onurcpgl <onurcpgl.34@hotmail.com>
2022-04-30 15:02:42 +0300

    Metotlar

---
+++ Metot/Metot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(Convert.ToInt32(sonuc));
+
+            Metotlar metot = new Metotlar();
+            metot.EkranaYazdir(Convert.ToString(sonuc));
+
+            Console.WriteLine(a+b);
+            int sonuc2 = metot.ArttırVeriTopla(a,b);
+
+        }
+        static int Topla(int a,int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public int ArttırVeriTopla(int a,int b)
+        {
+            a += 1;
+            b += 1;
+            return a + b;
+        }
+    }
+}
