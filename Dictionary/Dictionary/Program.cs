82390479d6b2122a3bf8c6a0b47d18041ab40aff
onurcpgl <onurcpgl.34@hotmail.com>
2022-05-03 16:05:11 +0300

    Dictionary

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Dictionary
+namespace dictionary
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+
+            kullanıcılar.Add(10,"Ayşe Yılmaz");
+            kullanıcılar.Add(12,"Ahmet Yılmaz");
+            kullanıcılar.Add(18,"Deniz Arda");
+            kullanıcılar.Add(20,"Özcan Çoşar");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("*** Elemanlara Erişim ***");
+
+            Console.WriteLine(kullanıcılar[12]); //12Anahtarına sahip elemanı getir.
+
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine("*** Count ***");
+            Console.WriteLine(kullanıcılar.Count);
+
+            //Contains
+            Console.WriteLine("*** Contains ***");
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("Zikriye Ürkmez"));
+
+            //Remove
+            Console.WriteLine("*** Remove ***");
+
+            Console.WriteLine(12);
+
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Keys
+            Console.WriteLine("*** Keys ***");
+            foreach (var item in kullanıcılar.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            //Value
+            Console.WriteLine("*** Value ***");
+            foreach (var item in kullanıcılar.Values)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
