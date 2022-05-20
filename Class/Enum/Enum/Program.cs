d6a1087f555468b5baae03b57df023f32329a35d
onurcpgl <onurcpgl.34@hotmail.com>
2022-05-20 16:32:37 +0300

    Enum

--- Class/Enum/Enum/Program.cs
+++ Class/Enum/Enum/Program.cs
@@ -6,7 +6,39 @@ namespace Enum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık = 27;
+            if (sıcaklık <=(int)HavaSıcaklıgı.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmaısnı bekleyin");
+
+            }
+            else if (sıcaklık >=(int)HavaSıcaklıgı.Sıcak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için sıcak bir gün");
+            }
+            else if (sıcaklık >=(int)HavaSıcaklıgı.Normal && sıcaklık<(int)HavaSıcaklıgı.Sıcak)
+            {
+                Console.WriteLine("Hadi Dışarı Çıkalım.. !! ");
+            }
         }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaSıcaklıgı
+    {
+        Soguk =5,
+        Normal=20,
+        Sıcak=30
     }
 }
