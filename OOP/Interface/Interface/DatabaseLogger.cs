906e316bfaea1f11bd46b045da8fea7a8bcefecc
onurcpgl <onurcpgl.34@hotmail.com>
2022-05-20 19:15:11 +0300

    Interface

---
+++ OOP/Interface/Interface/DatabaseLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class DatabaseLogger : ILogger
+    {
+        public void writeLog()
+        {
+            Console.WriteLine("Database'e yazar.");
+        }
+    }
+}
---
+++ OOP/Interface/Interface/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            DatabaseLogger databaseLogger = new DatabaseLogger();
+            databaseLogger.writeLog();
+
+            SmsLogger sms = new();
+            sms.writeLog();
+            //1.Yöntem.
+            FileLogger fileLogger = new FileLogger();
+            fileLogger.writeLog();
+            //2.Yöntem.
+            //LogManagera gönderip hepsini ordan çekebilirz tek tek uğraşmak yerine.
+            LogManager log = new LogManager(new FileLogger());
+            log.writeLog();
+        }
+    }
+}
