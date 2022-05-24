e881d73976ce73fc44e12bb95ca7ee13d94a7e08
onurcpgl <onurcpgl.34@hotmail.com>
2022-05-25 01:08:08 +0300

    Telefon Rehberi

---
+++ TelefonRehberi/TelefonRehberi/Kisi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonRehberi
+{
+    class Kisi
+    {
+        private string isim;
+        private string soyisim;
+        private int telno;
+
+        public void kisi(string ad,string soyisim,int no)
+        {
+            this.isim = ad;
+            this.soyisim = soyisim;
+            this.telno = no;
+        }
+        public void kisi()
+        {
+
+        }
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+        public string SoyIsim
+        {
+            get { return soyisim; }
+            set { soyisim = value; }
+        }
+        public int TelNo
+        {
+            get { return telno; }
+            set { telno = value; }
+        }
+    }
+}
